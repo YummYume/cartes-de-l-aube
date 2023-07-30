d8da4ef2b117698073833a849c1a1a071e06d503
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-07-30 21:27:21 +0000

    feat: add history test

--- api/test/plugins/plugins.test.js
+++ api/test/plugins/plugins.test.js
@@ -3,7 +3,6 @@ import { Schema, model } from 'mongoose';
 import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 
 import mongoose from '../../plugins/mongoose.js';
-import { apiInject } from '../utils';
 
 describe('Standalone plugins', () => {
   /**
@@ -11,40 +10,13 @@ describe('Standalone plugins', () => {
    */
   let app;
 
-  /**
-   * @type {ReturnType<typeof apiInject>}
-   */
-  let api;
-
   beforeEach(() => {
     app = Fastify();
-    api = apiInject(app);
   });
 
   afterEach(async () => {
     await app.close();
   });
-
-  // test('auth works standalone', async () => {
-  //   app.register(auth);
-
-  //   app.get('/auth-test', {
-  //     onRequest: app.auth([
-  //       async (request) => {
-  //         request.user = { username: 'test' };
-  //       },
-  //     ]),
-  //     handler: async (request, reply) => {
-  //       return reply.code(200).send(request.user);
-  //     },
-  //   });
-
-  //   await app.ready();
-
-  //   const res = await api.get('/auth-test');
-
-  //   expect(JSON.parse(res.body)).toBe({ username: 'test' });
-  // });
 
   test('mongoose works standalone', async () => {
     app.register(mongoose);
